a61dcce2e8776765eac775e80d79618eb90a9d24
darkside <raghuram.shankar98@gmail.com>
2023-08-20 10:54:37 +0100

    add rp construction and plot pie

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ pandas2ri.activate()
 # python packages
 import yfinance as yf
 from pandas_datareader import data as pdr
+import riskparityportfolio as rp
 import numpy as np
 import pandas as pd
 import datetime as dt
@@ -27,20 +28,21 @@ if len(packnames_to_install) > 0:
 
 
 # get ticker data
-def get_t(tickers, start, end):
+def get_t(
+    tickers,
+    start=dt.datetime.now() - dt.timedelta(days=10 * 365),
+    end=dt.datetime.now(),
+):
+    t_names = [
+        ticker + ": " + yf.Ticker(ticker).info["shortName"] for ticker in tickers
+    ]
     t_prices = pdr.get_data_yahoo(tickers, start, end)["Close"]
     t_returns = t_prices.resample("D").ffill().pct_change().dropna(axis=0)
-    return (t_prices, t_returns)
+    return (t_names, t_prices, t_returns)
 
 
 # fit mvt to get mean and covar of tickers
-def fit_mvt(
-    tickers,
-    start=dt.datetime.now() - dt.timedelta(days=365 * 10),
-    end=dt.datetime.now(),
-):
-    t_prices, t_returns = get_t(tickers=tickers, start=start, end=end)
-
+def fit_mvt(t_returns):
     # convert t to r dataframe
     data = pandas2ri.py2rpy_pandasdataframe(t_returns)
 
@@ -59,16 +61,25 @@ def fit_mvt(
     return results
 
 
+# construct risk parity/budgeting portfolio
+def construct_rbp(sigma, b):
+    return rp.vanilla.design(sigma, b).T
+
+
 if __name__ == "__main__":
-    # get ticker data
+    # choose tickers
     tickers = ["XRSG.L", "SPXP.L", "IWDG.L", "G500.L", "SWLD.L", "EQGB.L", "SGLN.L"]
 
-    results = fit_mvt(tickers)
+    # get ticker data
+    t_names, t_prices, t_returns = get_t(tickers=tickers)
+
+    # get mean and covar
+    results = fit_mvt(t_returns)
+
+    # construct risk parity portfolio
+    b = np.ones((len(tickers), 1)) * 1 / len(tickers)
+    weights = pd.Series(construct_rbp(results["cov"], b).flatten(), index=t_names).T
 
     # plot
     fig, ax = plt.subplots(figsize=(10, 10))
-    sns.heatmap(
-        pd.DataFrame(results["scatter"], index=tickers, columns=tickers),
-        annot=True,
-        ax=ax,
-    )
+    weights.plot.pie(autopct="%1.1f%%")
