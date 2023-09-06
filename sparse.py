4355bc903c462222e3b9c07a48d2112f458fd389
darkside <raghuram.shankar98@gmail.com>
2023-09-06 07:21:34 +0100

    add training only for some of the data

--- sparse.py
+++ sparse.py
@@ -9,7 +9,7 @@ from src.funcs import get_t, design_sparse
 if __name__ == "__main__":
     # choose tickers
     ticker_data = pd.read_json("ticker_data.json")
-    tickers_portfolio = ticker_data.loc[0:5, "tickers"]
+    tickers_portfolio = ticker_data.loc[0:6, "tickers"]
     ticker_index = ["RSP"]
 
     # get all combinations of n tickers
@@ -21,27 +21,35 @@ if __name__ == "__main__":
     crmse_df = pd.DataFrame(columns=c)
 
     # get all ticker data
-    start = dt.datetime(year=2020, month=1, day=1)
-    t_all_names, _, t_all_returns = get_t(tickers=tickers_portfolio, start=start)
-    _, _, t_index_returns = get_t(tickers=ticker_index, start=start)
+    start_test = dt.datetime(year=2020, month=1, day=1)
+    t_all_names, _, t_all_returns = get_t(tickers=tickers_portfolio, start=start_test)
+    _, _, t_index_returns = get_t(tickers=ticker_index, start=start_test)
 
     for comb in tickers_comb:
         # get comb ticker data
         t_portfolio_returns = t_all_returns.loc[:, comb]
-        t_portfolio_names, _, _ = get_t(tickers=comb, start=start)
+        t_portfolio_names, _, _ = get_t(tickers=comb, start=start_test)
 
         # ensure the same index
         starting_idx = max(t_portfolio_returns.index[0], t_index_returns.index[0])
         ending_idx = min(t_portfolio_returns.index[-1], t_index_returns.index[-1])
         t_index_returns = t_index_returns.loc[starting_idx:ending_idx]
         t_portfolio_returns = t_portfolio_returns.loc[starting_idx:ending_idx]
+
+        # train only until for some of the data
+        end_train = dt.datetime(year=2021, month=12, day=31)
+
         try:
             # design sparse portfolio
             w_sparse = design_sparse(
-                t_portfolio_returns, t_index_returns, l=1e-9, u=0.5, measure="ete"
+                t_portfolio_returns.loc[:end_train],
+                t_index_returns.loc[:end_train],
+                l=1e-9,
+                u=0.5,
+                measure="ete",
             )
 
-            # get dataframe with cumulative returns
+            # get dataframe with cumulative returns for all the data
             sparse_portfolio = dict()
             sparse_portfolio["sparse_" + ticker_index[0] + ":" + str(comb)] = (
                 np.array(
@@ -63,13 +71,8 @@ if __name__ == "__main__":
                 sparse_portfolio, index=t_portfolio_returns.index
             )
 
-            # get tracking error dictionary
-            crmse_d = dict()
-            for idx in range(num_tickers):
-                crmse_d["ticker_" + str(idx)] = t_portfolio_names[idx]
-                crmse_d["weight_" + str(idx)] = w_sparse[idx]
-
-            crmse_d["crmse"] = np.sqrt(
+            # get crmse
+            crmse = np.sqrt(
                 np.sum(
                     np.square(
                         sparse_portfolio["sparse_" + ticker_index[0] + ":" + str(comb)]
@@ -78,15 +81,26 @@ if __name__ == "__main__":
                 )
             )
 
-            # add to crmse dataframe
-            crmse_df = pd.concat(
-                (crmse_df, pd.DataFrame(crmse_d, index=[0])), ignore_index=True
-            )
+            # add to results only if
+            if crmse < 3.0:
+                # get tracking error dictionary
+                crmse_d = dict()
+                for idx in range(num_tickers):
+                    crmse_d["ticker_" + str(idx)] = t_portfolio_names[idx]
+                    crmse_d["weight_" + str(idx)] = w_sparse[idx]
 
-            # plot sparse index portfolio vs index returns
-            sparse_portfolio.plot.line(figsize=(12, 6))
+                crmse_d["crmse"] = crmse
 
-            plt.show()
+                # add to crmse dataframe
+                crmse_df = pd.concat(
+                    (crmse_df, pd.DataFrame(crmse_d, index=[0])), ignore_index=True
+                )
+
+                # plot sparse index portfolio vs index returns
+                sparse_portfolio.plot.line(figsize=(20, 6), ylabel="Return")
+                plt.axvline(x=end_train)
+
+                plt.show()
         except:
             pass
 
